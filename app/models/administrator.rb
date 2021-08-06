3ee80007c37ddc30bbbee960606088f9ca260072
appirits-amikaTakahashi <amika.takahashi@appirits.com>
2021-08-06 19:29:08 +0900

    refs #1 devise_コントローラー、モデル、ビューの追加

---
+++ app/models/administrator.rb
@@ -0,0 +1,12 @@
+class Administrator < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  VALID_PASSWORD_REGEX = /\A[a-z0-9]+\z/i
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true,
+                       length: { minimum: 8 }
+                       format: { with: VALID_PASSWORD_REGEX }
+end
