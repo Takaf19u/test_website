1ccac884334a5a8a3f718d31da0952b6b4d27035
appirits-amikaTakahashi <amika.takahashi@appirits.com>
2021-08-10 14:18:51 +0900

    mypage用コントローラー追加

--- app/models/user_information.rb
+++ app/models/user_information.rb
@@ -1,2 +1,5 @@
 class UserInformation < ApplicationRecord
+  belongs_to :user
+
+  validates :company_name, :name, :number, :user_id, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   devise_for :administrators, module: "administrators"
   devise_for :users, module: "users"
+
+  devise_scope :administrator do
+    root to: 'administrators/sessions#new'
+  end
 end
